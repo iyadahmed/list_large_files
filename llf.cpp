d6630de0eb78510e0186e9fc97d1850ec5db7a23
Iyad Ahmed <iyadahmed430@gmail.com>
2023-09-22 20:17:35 +0300

    C++ version

---
+++ llf.cpp
@@ -0,0 +1,31 @@
+#include <filesystem>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[])
+{
+  const char *directory;
+  if (argc == 1) {
+    directory = ".";
+  }
+  else {
+    directory = argv[1];
+  }
+
+  std::filesystem::path largest_file_path;
+  std::uintmax_t largest_file_size = 0;
+
+  for (const auto &entry : std::filesystem::recursive_directory_iterator(
+           directory, std::filesystem::directory_options::skip_permission_denied))
+  {
+    if (entry.is_regular_file()) {
+      const auto file_size = entry.file_size();
+      if (file_size > largest_file_size) {
+        largest_file_size = file_size;
+        largest_file_path = entry.path();
+      }
+    }
+  }
+
+  std::cout << largest_file_path << " " << largest_file_size << std::endl;
+}
