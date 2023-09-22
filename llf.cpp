04f01cc12290d2213785aae37312f489ffcac61e
Iyad Ahmed <iyadahmed430@gmail.com>
2023-09-22 20:40:24 +0300

    Reduce indentation a bit

--- llf.cpp
+++ llf.cpp
@@ -18,14 +18,14 @@ int main(int argc, char *argv[])
   for (const auto &entry : std::filesystem::recursive_directory_iterator(
            directory, std::filesystem::directory_options::skip_permission_denied))
   {
-    if (entry.is_regular_file()) {
-      const auto file_size = entry.file_size();
-      if (file_size > largest_file_size) {
-        largest_file_size = file_size;
-        largest_file_path = entry.path();
-      }
+    if (!entry.is_regular_file())
+      continue;
+    const auto file_size = entry.file_size();
+    if (file_size > largest_file_size) {
+      largest_file_size = file_size;
+      largest_file_path = entry.path();
     }
   }
 
-  std::cout << largest_file_path << " " << largest_file_size << std::endl;
+  std::cout << largest_file_path.stem() << " " << largest_file_size << std::endl;
 }
